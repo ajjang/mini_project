215e80ef867aa1b8cc8cbc1fa1647423ea7f0930
JANG <grace20124@naver.com>
2020-04-19 20:47:12 +0900

    add header

---
+++ manager.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <string.h>
+#include "product.h"
+int selectMenu();
+int selectProduct(Product* product, int count);
+int loadData(Product p[]);
+void saveData(Product *p, int count);
---
+++ product.h
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct Product{
+    char name[20];
+    int weight;
+    int price;
+    int sprice; //표준가격
+    int stars;
+}Product;
+
+
+
+//메뉴번호를 고르는 함수, return 매뉴 번호
+//int selectMenu();
+
+/*
+CRUD
+C: int addProduct(Product *p) : 성공적으로 Product가 추가된다면 return 1 
+R: void listProduct(Product *p[], int curcount),void readProduct(Product p) : 반복문을통해'readProduct'함수로 product정보를 출력한다.
+U: int updateScore(Product *p)
+D: int deleteProduct(Product *p)
+*/
+
+int addProduct(Product *p);
+void readProduct(Product p); 
+int updateProduct(Product *p);
+int deleteProduct(Product *p);
+void listProduct(Product *p, int curcount); 
+//Product 구조체 멤버정보로 product를 찾는다.
+//int selectProduct(Product *p, int count); 
+//void searchProduct(Product *p, int count);
+
+//File I/O
+void saveData(Product p[]);
+int loadData(Product p[]);
